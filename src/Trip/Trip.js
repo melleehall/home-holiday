b55375fb2c1d6a3a8949ce530cc12405c9600304
Melanie Hallowell <melleehall@gmail.com>
2020-05-12 22:07:56 -0600

    Added delete button to trips and enabled function with context and DELETE method on /trips/:trip_id endpoint

--- src/Trip/Trip.js
+++ src/Trip/Trip.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import config from '../config'
+import TripsContext from '../TripsContext'
 import './Trip.css'
 
 export default class Trip extends Component {
@@ -34,7 +35,6 @@ export default class Trip extends Component {
                 throw error
               })
             }
-            return res.json()
         })
         .then(data => {
             this.setState({
@@ -58,13 +58,55 @@ export default class Trip extends Component {
           is_taken: !prevState.is_taken,
         }));
     }
-
     render () {
-        console.log(`${this.props.id}: ${this.props.is_taken}`)
+
+    
+    function deleteTripRequest(tripId, cb) {
+        console.log(`${config.API_BASE_URL}trips/${tripId}`)
+        fetch(`${config.API_BASE_URL}trips/${tripId}`, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(res => {
+            if (!res.ok) {
+              return res.json().then(error => {
+                throw error
+              })
+            }
+          })
+        .then(data => {
+            cb(tripId)
+        })
+        .catch(e => {
+            console.error(e)
+        })
+    }
+
+        const id = this.props.id
 
         return (
             <li className='trip-item'>
-                <h2 className='trip-heading'>{this.props.trip_name}</h2>
+                <div className='trip-header-container'>
+                    <h2 className='trip-heading'>{this.props.trip_name}</h2>
+                    <TripsContext.Consumer>
+                            {(context) => {
+                                return <button 
+                                    className="delete-trip" 
+                                    type="button"
+                                    onClick={() => {
+                                    deleteTripRequest(
+                                        id,
+                                        context.removeTripFromState,
+                                    )
+                                    }}>                   
+                                    <svg aria-hidden="true" data-prefix="fas" data-icon="trash-alt" className="trashcan" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M0 84V56c0-13.3 10.7-24 24-24h112l9.4-18.7c4-8.2 12.3-13.3 21.4-13.3h114.3c9.1 0 17.4 5.1 21.5 13.3L312 32h112c13.3 0 24 10.7 24 24v28c0 6.6-5.4 12-12 12H12C5.4 96 0 90.6 0 84zm416 56v324c0 26.5-21.5 48-48 48H80c-26.5 0-48-21.5-48-48V140c0-6.6 5.4-12 12-12h360c6.6 0 12 5.4 12 12zm-272 68c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208z"></path></svg> 
+                                    Remove
+                                </button>                        
+                            }} 
+                    </TripsContext.Consumer>
+                </div>
                 <p>{this.props.description}</p>
                 <ul className='trip-elements'>
                     <li> <span className='sense-type'>{this.props.sense_one}</span> <button><a target='_blank' rel="noopener noreferrer" href={this.props.resource_one} className='trip-resource'>Visit Element Resource</a></button> <span className='trip-element'>{this.props.element_one}</span></li>
@@ -87,9 +129,9 @@ export default class Trip extends Component {
                         id='trip_complete'
                     />
                     <button type='submit'>Save</button>
-                <p className='success_msg'>
-                    <b>{this.state.success_msg ? 'Trip status updated!' : '' }</b> 
-                </p>
+                    <p className='success_msg'>
+                        <b>{this.state.success_msg ? 'Trip status updated!' : '' }</b> 
+                    </p>
                 </form>
                 <div className='separation-div'></div>
             </li>
